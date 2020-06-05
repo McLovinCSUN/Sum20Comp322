a78052bb1c806adf73ac23cf21b8ed9060ca357c
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2020-06-05 11:27:01 -0700

    Update
    used open(), write(), read() calls to create files and copy from file 1 into file 2.

--- Lab1/fileCopy.c
+++ Lab1/fileCopy.c
@@ -1,51 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<fcntl.h> 
+#include <unistd.h>
+#include<string.h>
+
+#define SIZE 1024
 
 int main(){
-    char fileName[100];
-    char copy,c;
-
-    printf("Please enter file name: ");
-    scanf("%s", fileName);
+    char file1[SIZE];
+    char file2[SIZE];
+    char input1[]="Hello world!";
+    int inputLen =strlen(input1);
+    int fp,fp2,i;
+    int copyNum= inputLen - (inputLen-1);
 
     //creates file one
-    FILE *fp= fopen(fileName, "r" ); 
+    printf("Please enter input file name: ");
+    scanf("%s", file1);
+    printf("\n");
+
+    fp= open(file1, O_RDWR | O_EXCL|   O_CREAT);  
     
-    if (fp == NULL){
+    if (fp == -1){
         printf("error.\n");
-        exit (0);
     }
+    write(fp,input1,inputLen);
 
-    //input something random into file one 
-    fprintf(fp, "This is what is inside file one.\n"); 
+    //creates a file2
+    printf("Please enter output file name: ");
+    scanf("%s", file2);
+    printf("\n");
     
-    //creates a file
-    FILE *fp2= fopen("file2", "w" ); 
+    fp2= open(file2, O_WRONLY | O_EXCL| O_CREAT);
     
-    if (fp2 == NULL){
-        printf("error.\n");
-        exit (0);
+     if (fp2 == -1){
+        printf("error.\n"); 
     }
-
-    //read from file one and copy its contents into to file 2
-    copy = fgetc(fp);
-    while (copy !=EOF){
-        fputc(copy,fp2);
-        copy = fgetc(fp);
+ 
+    //copies file1 contents into file2
+    for(i=0; i< copyNum;i++){
+        read(fp,input1,inputLen);
+        write(fp2,input1,inputLen);
     }
-
-    //write into file two
-    fprintf(fp2, "File one copied into file two.\n");
-
-    fclose(fp);
-    fclose(fp2);
-
-    //reads what is inside file two
-    fp2 = fopen("copyFile", "r+");
-    while ((c = getc(fp2)) != EOF) {
-        printf("%c", c);
-     }
-     fclose(fp2);
-
+    
+    close(fp);
+    close(fp2);
      return 0;
 }
