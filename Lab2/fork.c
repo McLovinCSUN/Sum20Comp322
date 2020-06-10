038b7c00680452684ff1af77bf39963087c01446
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2020-06-10 12:12:11 -0700

    Update fork.c
    Adjusted for the parent to wait for all the children process to finish before printing out its printf statement

--- Lab2/fork.c
+++ Lab2/fork.c
@@ -5,35 +5,42 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-    int i,child;
+    int i;
     int counter = 0;
     //convert string type to int using the atoi function
     int j = atoi(argv[1]);
 
+    //fork is creating a new process by copying the calling process (parent)
+    //at the time of fork() both memory spaces have the same content.
+    pid_t child = fork();
+    //after the fork, operations performed by the processes do not effect one another
+
     //this loop will execute however number of times inputed by user
-    for(i = 0; i < j; i++){
-        //fork is creating a new process by copying the calling process (parent)
-        //at the time of fork() both memory spaces have the same content.
-        child = fork();
-        //after the fork, operations performed by the processes do not effect one another
-
-        //if the the child process is less than zero the parent was not able to create a child process
-        //then it exits 
-        if (child < 0){
-            printf("Error.");
-            exit (0);
+    if(child == 0){
+        for(i = 0; i < j; i++){
+            //if the the child process is less than zero the parent was not able to create a child process
+            //then it exits 
+            if (child < 0){
+                printf("Error.");
+                exit (0);
+            }else{
+                //if the parent successed in creating a child process, the child PID is returned to the parent
+                //and the child get back a zero. that is how you know it is a child process.
+                counter= i+1;
+                printf("Hello World from child %d\n", counter);   
+            }
         }
-
-        //if the parent successed in creating a child process, the child PID is returned to the parent
-        //and the child get back a zero. that is how you know it is a child process. 
-        if(child == 0){
-            counter= i+1;
-            printf("Hello World from child %d\n", counter);   
-        }else{
-            //the parent is going to wait till child process j terminates 
-            wait(&j);
-        }
+    }else{
+        //the parent is going to wait till child process j terminates
+        waitpid(child,&j,0);
+        printf("Parent processor of is done!\n");
     }
-
-  return (0);
+    return (0);
 }
+
+//tips from professor 
+// if the parent can print an statement at the end it would be great 
+// This parent should be good to its children and wait() for all of them to finish executing. 
+// I can accept it, however it is better to print only ONE statement  
+// currently it shows that the parent waits for all the childeren
+// which is sufficient for this requirement : This parent should be good to its children and wait() for all of them to finish executing. 
